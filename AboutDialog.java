6b0fb8717a737db6e84d92dfbd91e03e9da88947
Dhanasri1609 <138751434+Dhanasri1609@users.noreply.github.com>
2024-06-03 13:41:21 +0530

    Add files via upload

---
+++ AboutDialog.java
@@ -0,0 +1,14 @@
+package text;
+
+public class AboutDialog {
+
+	public AboutDialog(Texteditor texteditor, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ balls.java
@@ -0,0 +1,6 @@
+package text;
+public class balls {
+    public static void main(String args[]) {
+        Texteditor to = new Texteditor();
+    }
+}
